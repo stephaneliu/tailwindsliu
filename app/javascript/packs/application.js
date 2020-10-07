16fd064cb3261dc19d1bd3e94a128e8b714aaefa
Stephane Liu <sliu@sjliu.com>
2020-10-06 19:55:48 -1000

    Chore: Configure heroku

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Tailwind CSS
+import "stylesheets/application"
