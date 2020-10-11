b408e8618c6f92af367469137956a8909cee9d03
Stephane Liu <sliu@sjliu.com>
2020-10-11 10:11:13 -1000

    Add alpinejs
      - yarn add alpinejs
      - yarn add @tailwindcss/ui

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
+require("alpinejs")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -18,3 +18,4 @@ require("channels")
 
 // Tailwind CSS
 import "stylesheets/application"
+import "../stylesheets/application.scss"
